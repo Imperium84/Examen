90f232941aa5dc59593d73af616ffaf9420cca1f
Antoine Cornwell <cornwell.a.euroa2016@gmail.com>
2019-05-28 11:02:32 +0200

    Exo2

---
+++ exo2.php
@@ -0,0 +1,46 @@
+<?php
+
+class ArrayManager
+{
+    private $tableau;
+    
+    function __construct(array $tab)
+    {
+        $this->tableau = $tab;
+    }
+    
+    function getTableau()
+    {
+        return $this->tableau;
+    }
+    
+    function setTableau(array $tab)
+    {
+        $this->tableau = $tab;
+    }
+    
+    //Méthode permettant de trouver le plus petit nombre du tableau
+    function trouverPetit()
+    {
+        $taille = count($this->tableau);
+        $index = 0;
+        $nombreCourant = 0;
+        $nombrePetit = 0;
+
+        for ($index = 0; $index<$taille; $index++){
+
+            $nombreCourant = $this->tableau[$index];
+
+            if ($index == 0){
+                $nombrePetit=$nombreCourant;
+            }
+
+            if ($nombrePetit > $nombreCourant){
+                $nombrePetit = $nombreCourant;
+            }
+        }
+    
+        return $nombrePetit;
+    }
+    
+}
