2afce82396bea68dda4a16a9cc85e78c9e2c5280
Antoine Cornwell <cornwell.a.euroa2016@gmail.com>
2019-05-28 11:08:53 +0200

    Exo2

--- exo2.php
+++ exo2.php
@@ -1,52 +1,49 @@
 <?php
 
 /* Classe permettant de gérer les tableaux
- * Contient un constructeur
+ * Contient un 
  * Contient un getter
  * Contient un setter
  * Contient une méthode renvoyant le nombre le plus petit présent dans le tableau
  */
-class ArrayManager
-{
+
+class ArrayManager {
+
     private $tableau;
-    
-    function __construct(array $tab)
-    {
+
+    function __construct(array $tab) {
         $this->tableau = $tab;
     }
-    
-    function getTableau()
-    {
+
+    function getTableau() {
         return $this->tableau;
     }
-    
-    function setTableau(array $tab)
-    {
+
+    function setTableau(array $tab) {
         $this->tableau = $tab;
     }
-    
+
     //Méthode permettant de trouver le plus petit nombre du tableau
-    function trouverPetit()
-    {
+    function trouverPetit() {
         $taille = count($this->tableau);
         $index = 0;
         $nombreCourant = 0;
         $nombrePetit = 0;
 
-        for ($index = 0; $index<$taille; $index++){
+        for ($index = 0; $index < $taille; $index++) {
 
             $nombreCourant = $this->tableau[$index];
 
-            if ($index == 0){
-                $nombrePetit=$nombreCourant;
+            if ($index == 0) {
+                $nombrePetit = $nombreCourant;
             }
 
-            if ($nombrePetit > $nombreCourant){
+            if ($nombrePetit > $nombreCourant) {
                 $nombrePetit = $nombreCourant;
             }
         }
-    
+
         return $nombrePetit;
     }
-    
+
 }
